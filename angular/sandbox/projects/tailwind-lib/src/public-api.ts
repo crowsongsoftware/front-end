f7c7afa1c2b11865b0de608b1cc87d1a6c5f47b8
lilmick <mcmahonmichael56@gmail.com>
2021-07-15 18:33:15 -0400

    trying to update development-workspace angular cli

--- angular/sandbox/projects/tailwind-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of tailwind-lib
- */
-
-export * from './lib/tailwind-lib.service';
-export * from './lib/tailwind-lib.component';
-export * from './lib/tailwind-lib.module';
