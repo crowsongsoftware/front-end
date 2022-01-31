5effbeda5d1c94347eb6451ceb7429d0826dc9c7
lilmick <mcmahonmichael56@gmail.com>
2022-01-31 09:00:07 -0500

    commiting for angular update

---
+++ angular/content-management-workspace/projects/content-management-system/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'Your api key',
+    authDomain: 'auth name',
+    databaseURL: 'database url',
+    projectId: 'project id',
+    storageBucket: 'bucket',
+    messagingSenderId: 'msg id',
+    appId: 'app id',
+    measurementId: 'msr id'
+  }
+};
